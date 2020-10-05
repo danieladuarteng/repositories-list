5b9fb89c2d208b148beca0314ebfe2504d9b96d4
Daniela Duarte <falecom@danieladuarte.com.br>
2020-10-05 20:16:14 -0300

    Updates

---
+++ precache-manifest.16cdff349e8e724e0ad174fb20a438ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb4f21450c4ab6d077b7e59f5333e8f",
+    "url": "/repositories-list/index.html"
+  },
+  {
+    "revision": "6d454bd9970ce4415bfc",
+    "url": "/repositories-list/static/js/2.aa7257c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repositories-list/static/js/2.aa7257c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365606b1cb7624bbbe30",
+    "url": "/repositories-list/static/js/main.4a318b5b.chunk.js"
+  },
+  {
+    "revision": "7d2804da8302aceaefec",
+    "url": "/repositories-list/static/js/runtime-main.99c6e0de.js"
+  }
+]);
